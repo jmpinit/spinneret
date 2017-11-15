4187b87eb0b2a7e133ee22f0b964b09eb7e8456c
Owen Trueblood <hi@owentrueblood.com>
2017-11-15 07:35:49 -0500

    Load image as a canvas

--- src/get.js
+++ src/get.js
@@ -17,4 +17,30 @@ function get(url) {
   });
 }
 
-module.exports = get;
+function imageAsCanvas(url, width, height) {
+  const imageTag = document.createElement('img');
+  imageTag.src = url;
+
+  return new Promise((fulfill, reject) => {
+    imageTag.onerror = (event) => {
+      reject(new Error(event.msg));
+    };
+
+    imageTag.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = width || imageTag.width;
+      canvas.height = height || imageTag.height;
+
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(imageTag, 0, 0, canvas.width, canvas.height);
+
+      fulfill(canvas);
+    };
+  });
+}
+
+
+module.exports = {
+  raw: get,
+  imageAsCanvas,
+};
